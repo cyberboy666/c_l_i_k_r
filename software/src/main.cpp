e6a5c994d5987ae2c9e47b97f9f0cbb2f089b16d
cyberboy666 <langolierz@gmail.com>
2022-10-27 20:04:00 +1300

    made pot holes larger

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -20,10 +20,10 @@ int a0 = A7;
 int a1 = A8;
 int a2 = A9;
 int a3 = A10;
-int a4 = A0;
-int a5 = A1;
-int a6 = A2;
-int a7 = A3;
+int a4 = A2;
+int a5 = A3;
+int a6 = A0;
+int a7 = A1;
 
 int v0, v1, v2, v3, v4, v5, v6, v7;
 int n0, n1, n2, n3, n4, n5, n6, n7;
@@ -36,46 +36,44 @@ void sendCC(uint8_t channel, uint8_t control, uint8_t value) {
 }
 
 void checkAndSendValue(int old_analog_value, int new_analog_value, uint8_t control) {
-  if (abs(old_analog_value - new_analog_value) > 1){
-      sendCC(0,control,new_analog_value / 4);
+  if(old_analog_value != new_analog_value){
+      sendCC(0,control,new_analog_value);
   }
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Keyboard.begin();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  char key = keypad.getKey();
-    v0 = analogRead(a0);
+    v0 = analogRead(a0)/8;
     checkAndSendValue(n0, v0, 0);
     n0 = v0;
-    v1 = analogRead(a1);
+    v1 = analogRead(a1)/8;
     checkAndSendValue(n1, v1, 1);
     n1 = v1;
-    v2 = analogRead(a2);
+    v2 = analogRead(a2)/8;
     checkAndSendValue(n2, v2, 2);
     n2 = v2;
-    v3 = analogRead(a3);
+    v3 = analogRead(a3)/8;
     checkAndSendValue(n3, v3, 3);
     n3 = v3;
-    v4 = analogRead(a4);
+    v4 = analogRead(a4)/8;
     checkAndSendValue(n4, v4, 4);
     n4 = v4;
-    v5 = analogRead(a5);
+    v5 = analogRead(a5)/8;
     checkAndSendValue(n5, v5, 5);
     n5 = v5;
-    v6 = analogRead(a6);
+    v6 = analogRead(a6)/8;
     checkAndSendValue(n6, v6, 6);
     n6 = v6;
-    v7 = analogRead(a7);
+    v7 = analogRead(a7)/8;
     checkAndSendValue(n7, v7, 7);
     n7 = v7;
 
+  char key = keypad.getKey();
 
   if (key != NO_KEY){
      Keyboard.write(key);
